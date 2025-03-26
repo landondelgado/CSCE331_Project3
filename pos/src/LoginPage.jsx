fb27152ed4ad7ce06e275010cf4f60e0fceb9657
landondelgado <landon.delgado@gmail.com>
2025-03-26 14:35:29 -0500

    Set up login page frontend

--- pos/src/LoginPage.jsx
+++ pos/src/LoginPage.jsx
@@ -1,21 +1,34 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import customerIcon from './images/customer.png';
 import './default_styles.css';
 
 function LoginPage() {
     const navigate = useNavigate();
 
-    const handleLogin = (e) => {
+    const goToPOSMenu = (e) => {
         e.preventDefault();
-        // You can add auth logic here later
-        navigate('/menu'); // Navigate to MainMenu
+        // Add auth logic here later
+        navigate('/menupos');
     };
+
+    const goToCustomerInterface = (e) => {
+        e.preventDefault();
+        navigate('/menu');
+    }
 
     return (
     <div className="main-container">
+        <div className="customer-div">
+            <button className="image-button" onClick={goToCustomerInterface}>
+                <img src={customerIcon} alt="Customer Interface" className="button-icon" />
+                <span className="button-text">Customer Interface</span>
+            </button>
+        </div>
+
         <div className="login-box">
-            <h2 className="login-title">Welcome Back</h2>
-            <form className="login-form" onSubmit={handleLogin}>
+            <h2 className="login-title">Log in to Register</h2>
+            <form className="login-form" onSubmit={goToPOSMenu}>
                 <input type="text" placeholder="Username" className="login-input" />
                 <input type="password" placeholder="Password" className="login-input" />
                 <button type="submit" className="login-button">Login</button>
---
+++ pos/src/MenuPOS.jsx
@@ -0,0 +1,13 @@
+// MenuPOS.jsx
+import React from 'react';
+
+function Menu() {
+  return (
+    <div>
+      <h1>Menu</h1>
+      <p>This is the point of sales menu page.</p>
+    </div>
+  );
+}
+
+export default Menu;
