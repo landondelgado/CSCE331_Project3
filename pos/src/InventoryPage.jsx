4cfb054c19bf9327c6c51a2c35ed26ca83fe8fcf
hazcazdaz <hazemhhasalem@gmail.com>
2025-03-30 20:06:42 -0500

    Initilization & Properly tested path so the page shows up

---
+++ pos/src/InventoryPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function InventoryPage() {
+    return(
+        <div>
+            {/* Fill this with the tailwind layout & styling later*/}
+            <h1> Inventory Page (Placeholder)</h1>
+        </div>
+    )
+}
