1d962af0f480277bcde31aa79a4d52ce1c28a588
hazcazdaz <hazemhhasalem@gmail.com>
2025-03-30 20:24:53 -0500

    Header layout frontend initialized

--- pos/src/InventoryPage.jsx
+++ pos/src/InventoryPage.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
+import bobabackground from './images/bobabackground.svg';
 
 export default function InventoryPage() {
     return(
-        <div>
+    <div 
+        className = "min-h-screen bg-blue-900 text-white bg-cover bg-no-repeat"
+        style = {{ backgroundImage: `url(${bobabackground})`}}
+        >
+        
+        {/*Header*/}
+        <header className = "flex items-center justify-between p-4 bg-blue-800">
+            {/* Left: Navigation Buttons */}
+            <div className = "flex gap-4">
+                <button className = "hover:text-gray-300">Menu</button>
+                <button className = "hover:text-gray-300">Inventory</button>
+                <button className = "hover:text:-gray-300">Analytics</button>
+            </div>
+            {/* Center: Logo and Title */}
+            <div className = "text-center">
+                <h1 className = "text-xl font-bold">Sharetea</h1>
+                <p className = "text-sm">Inventory</p>
+            </div>
+            {/* Right: Time and Logout */}
+            <div className = "text-right">
+                <p className = "text-sm">04:35:32 PM</p>
+                <button className = "hover:text-gray-300">Logout</button>
+            </div>
+        </header>
+        {/* Main Content: Placeholder */}
+        <main className = "p-4">
+            <h2 className = "text-2x1 font-bold"> Inventory Page </h2>
             {/* Fill this with the tailwind layout & styling later*/}
-            <h1> Inventory Page (Placeholder)</h1>
-        </div>
+            <p>Inventory content goes here.</p>
+        </main>
+        {/* Footer */}
+    </div>
     )
 }
