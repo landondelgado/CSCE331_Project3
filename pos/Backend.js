182c2c32ca296cd4ac2f8023f52cdc18c6841edc
landondelgado <landon.delgado@gmail.com>
2025-04-08 12:56:25 -0500

    Make Backend Render Compatible

--- pos/Backend.js
+++ pos/Backend.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use('/api/inventory', inventoryBackend);
 
 // Open backend on port 3001
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
